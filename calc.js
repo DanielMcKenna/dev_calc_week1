a9a62d5fb9f02bec4d1a8a4d807093ad851209e8
Daniel McKenna <mckenna.daniel15@gmail.com>
2021-09-25 12:37:44 -0600

    finished calculater

---
+++ calc.js
@@ -0,0 +1,47 @@
+const readline = require('readline');
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+
+
+reader.question("What would you like to calculate?", function(input){
+	tokens = input.toLowerCase().split(' ');
+	
+	mathSymbol = tokens[0];
+	num1 = Number(tokens[1]);
+	num2 = Number(tokens[2]);
+
+	if (mathSymbol === '+'){
+		console.log(num1 + num2);
+	}
+	else if(mathSymbol === '-'){
+		if(num1 > num2){
+			console.log(num1 - num2);
+		}
+		else if(num1 < num2){
+			console.log(num2 - num1);
+		}
+		else{
+			console.log(num1 - num2);
+		}
+	}
+	else if(mathSymbol === '*' || mathSymbol === 'x'){
+		console.log(num1 * num2);
+	}
+	else if(mathSymbol === '/' || mathSymbol === 'divide') {
+		console.log(num1 / num2);
+	}
+	else if(mathSymbol === '[]' || mathSymbol === 'square') {
+		console.log (num1 * num1);
+	}
+	else{
+		console.log("incorrect input try again");
+	}
+
+	// This line closes the connection to the command line interface.
+	reader.close()
+
+});
